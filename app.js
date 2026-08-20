4d63dded85fcb9a772633c8ce3b720d34a5b2fbe
Jhon Anthony Pulupa Espinoza <jae.pulupa@yavirac.edu.ec>
2026-08-19 20:20:05 -0500

    Primer commit: conexion a MongoDB Atlas

---
+++ app.js
@@ -0,0 +1,28 @@
+require('dotenv').config(); // NUEVO
+
+const express = require('express');
+const mongoose = require('mongoose'); // NUEVO
+
+const app = express();
+
+const PORT = 3000;
+
+// Permitir que el servidor reciba datos en formato JSON
+app.use(express.json());
+
+// Conexión a MongoDB Atlas // NUEVO
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log('✅ Conectado a MongoDB Atlas'))
+    .catch((err) => console.error('❌ Error de conexión:', err));
+
+// Ruta principal
+app.get('/', (req, res) => {
+    res.json({
+        mensaje: 'Servidor de Teatro Sing funcionando correctamente 🎭'
+    });
+});
+
+// Iniciar servidor
+app.listen(PORT, () => {
+    console.log(`Servidor Teatro Sing ejecutándose en http://localhost:${PORT}`);
+});
